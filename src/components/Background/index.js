ef32085afa9bdd71487068155a168c8148742d39
Henrique Tavares <ihenrits@gmail.com>
2020-03-21 22:07:29 -0300

    feat: creating Background component

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#7159c1'],
+})`
+  flex: 1;
+`;
