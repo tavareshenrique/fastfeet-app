2b10045160b4fb719bc70cf01ba21bededac74cc
Henrique Tavares <ihenrits@gmail.com>
2020-03-21 22:07:59 -0300

    feat: creating delivery page

---
+++ src/pages/Delivery/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Delivery() {
+  // const data = useSelector((state) => state.auth.data);
+
+  // useEffect(() => {
+  //   console.tron.log('data', data);
+  // }, [data]);
+
+  return (
+    <View>
+      <Text>Delivery</Text>
+    </View>
+  );
+}
