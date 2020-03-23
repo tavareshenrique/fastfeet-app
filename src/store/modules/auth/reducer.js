2d67397aa41e3fd945e08a6b5864a703cb5dddab
Henrique Tavares <ihenrits@gmail.com>
2020-03-23 09:48:01 -0300

    refactor: code indentation

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,13 +1,13 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  data: null,
+  data: [],
   signed: false,
   loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  return produce(state, (draft) => {
+  return produce(state, draft => {
     switch (action.type) {
       case '@auth/SIGN_IN_REQUEST': {
         draft.loading = true;
