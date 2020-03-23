d97638732544de35df22723adb1ca59f95c7347e
Henrique Tavares <ihenrits@gmail.com>
2020-03-23 09:44:40 -0300

    feat: create DeliveryItem component

---
+++ src/components/DeliveryList/DeliveryItem/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Icons from 'react-native-vector-icons/MaterialIcons';
+
+import Timeline from '~/components/Timeline';
+
+import {
+  ContentDelivery,
+  HeaderDelivery,
+  TitleContent,
+  TitleProduct,
+  FooterDelivery,
+  FooterContent,
+  DataDelivery,
+  CityDelivery,
+  ViewDetail,
+  Label,
+  DataContent,
+  CityContent,
+} from './styles';
+
+export default function DeliveryItem({ data }) {
+  function elevationShadowStyle(elevation) {
+    return {
+      elevation,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 0.5 * elevation },
+      shadowOpacity: 0.3,
+      shadowRadius: 0.8 * elevation,
+    };
+  }
+
+  const styles = StyleSheet.create({
+    shadow: { ...elevationShadowStyle(5), backgroundColor: 'white' },
+  });
+
+  function statusDelivery(dataStatus) {
+    if (dataStatus.end_date) {
+      return 'done';
+    }
+    if (dataStatus.start_date) {
+      return 'progress';
+    }
+
+    return 'pedding';
+  }
+
+  return (
+    <ContentDelivery style={styles.shadow}>
+      <HeaderDelivery>
+        <TitleContent>
+          <Icons name="local-shipping" size={30} color="#8f71ea" />
+          <TitleProduct>{data.product}</TitleProduct>
+        </TitleContent>
+
+        <Timeline status={statusDelivery(data)} />
+      </HeaderDelivery>
+
+      <FooterDelivery>
+        <FooterContent>
+          <DataContent>
+            <Label>Data</Label>
+            <DataDelivery>
+              {format(
+                new Date(data.recipient.createdAt),
+                'dd/MM/yyyy',
+                {
+                  locale: pt,
+                },
+              )}
+            </DataDelivery>
+          </DataContent>
+          <CityContent>
+            <Label>Cidade</Label>
+            <CityDelivery>{data.recipient.city}</CityDelivery>
+          </CityContent>
+          <TouchableOpacity>
+            <ViewDetail>Ver detalhes</ViewDetail>
+          </TouchableOpacity>
+        </FooterContent>
+      </FooterDelivery>
+    </ContentDelivery>
+  );
+}
+
+DeliveryItem.propTypes = {
+  data: PropTypes.shape({
+    product: PropTypes.string,
+    recipient: PropTypes.shape({
+      createdAt: PropTypes.string,
+      city: PropTypes.string,
+    }),
+  }).isRequired,
+};
---
+++ src/components/DeliveryList/DeliveryItem/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components/native';
+
+export const ContentDelivery = styled.View`
+  margin: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+`;
+
+export const HeaderDelivery = styled.View`
+  padding: 20px;
+`;
+
+export const TitleContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const TitleProduct = styled.Text`
+  color: #8f71ea;
+  font-weight: bold;
+  font-size: 18px;
+  margin-left: 10px;
+`;
+
+export const FooterDelivery = styled.View`
+  background: #f8f9fd;
+`;
+
+export const FooterContent = styled.View`
+  padding: 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Label = styled.Text`
+  color: #a6a6a7;
+  font-size: 12px;
+`;
+
+export const DataContent = styled.View`
+  flex-direction: column;
+`;
+
+export const CityContent = styled.View`
+  flex-direction: column;
+`;
+
+export const DataDelivery = styled.Text`
+  color: #444444;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+export const CityDelivery = styled.Text`
+  color: #444444;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+export const ViewDetail = styled.Text`
+  font-weight: bold;
+  font-size: 15px;
+  color: #8f71ea;
+`;
