2a5613792500fac17aa3841a8d425883f2c477bb
Henrique Tavares <ihenrits@gmail.com>
2020-03-26 20:22:55 -0300

    refactor: code refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import { useSelector } from 'react-redux';
 import createRouter from './routes';
 
 export default function App() {
-  const signed = useSelector(state => state.auth.signed);
+  const signed = useSelector((state) => state.auth.signed);
   return createRouter(signed);
 }
