56c99b9ce262a9e30f9ddbf5338f2723956c4eb5
Henrique Tavares <ihenrits@gmail.com>
2020-03-26 20:19:23 -0300

    feat: add react native splash screen for android

--- android/app/src/main/java/com/fastfeetapp/MainActivity.java
+++ android/app/src/main/java/com/fastfeetapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fastfeetapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +16,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "fastfeetapp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   @Override
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-// import { StatusBar } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+
+import SplashScreen from 'react-native-splash-screen';
 
 import './config/ReactotronConfig';
 
@@ -13,11 +14,16 @@ import { store, persistor } from './store';
 import App from './App';
 
 export default function Index() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
+  }, []);
+
   return (
     <NavigationContainer>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          {/* <StatusBar barStyle="light-content" backgroundColor="#7159c1" /> */}
           <App />
         </PersistGate>
       </Provider>
