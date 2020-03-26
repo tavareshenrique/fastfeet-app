396fbbf6950272603e2ac792f1b647475d730e5f
Henrique Tavares <ihenrits@gmail.com>
2020-03-25 21:05:37 -0300

    feat: creating profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 
-// import { Container } from './styles';
+import Avatar from '~/components/Avatar';
+import Button from '~/components/Button';
+
+import {
+  Container,
+  ContentAvatar,
+  DataProfile,
+  ContentProfile,
+  LabelInformation,
+  LabelData,
+} from './styles';
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <Container>
+      <ScrollView>
+        <ContentAvatar>
+          <Avatar size={150} name="Henrique Tavares" />
+        </ContentAvatar>
+
+        <DataProfile>
+          <ContentProfile>
+            <LabelInformation>Nome Completo</LabelInformation>
+            <LabelData>Henrique Tavares</LabelData>
+          </ContentProfile>
+
+          <ContentProfile>
+            <LabelInformation>Email</LabelInformation>
+            <LabelData>ihenrits@gmail.com</LabelData>
+          </ContentProfile>
+
+          <ContentProfile>
+            <LabelInformation>Data de Cadastro</LabelInformation>
+            <LabelData>10/01/2020</LabelData>
+          </ContentProfile>
+        </DataProfile>
+
+        <Button color="#e74040" loading={false}>
+          Sair
+        </Button>
+      </ScrollView>
+    </Container>
   );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  background: #fff;
+  flex: 1;
+  padding: 30px;
+`;
+
+export const ContentAvatar = styled.View`
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const DataProfile = styled.View`
+  align-items: flex-start;
+  margin-bottom: 20px;
+`;
+
+export const ContentProfile = styled.View`
+  margin: 10px;
+`;
+
+export const LabelInformation = styled.Text`
+  color: #797979;
+  font-size: 18px;
+`;
+
+export const LabelData = styled.Text`
+  color: #444444;
+  font-weight: bold;
+  font-size: 21px;
+`;
