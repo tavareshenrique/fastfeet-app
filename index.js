b5fffef2c6d93a27c2e296f8ee3616fa0774dd56
Henrique Tavares <ihenrits@gmail.com>
2020-04-04 21:32:46 -0300

    chore: add unstable enableLogBox

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
+
+require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
